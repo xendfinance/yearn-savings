0877812862162eddbd820a01242696eeaa22969e
Njoku Emmanuel <kalunjoku123@gmail.com>
2020-10-27 08:50:58 +0100

    still on xend individual test

--- test/XendFinanceIndividual_Yearn_V1.js
+++ test/XendFinanceIndividual_Yearn_V1.js
@@ -2,6 +2,7 @@ const { assert } = require("console");
 const GroupsContract = artifacts.require("Groups");
 const TreasuryContract = artifacts.require("Treasury");
 const CyclesContract = artifacts.require("Cycles");
+const utils = require("./helpers/utils");
 const ClientRecordContract = artifacts.require("ClientRecord");
 const SavingsConfigContract = artifacts.require("SavingsConfig");
 const DaiLendingAdapterContract = artifacts.require("DaiLendingAdapter");
@@ -55,10 +56,57 @@ contract("XendFinanceIndividual_Yearn_V1", async (accounts) => {
     assert(contractInstance.address !== "");
   });
 
-  it("should get client records", async () => {
+  it("should throw error because no client records exist", async () => {
       
-      const getClientRecordResult = await contractInstance.getClientRecord(accounts[1]);
-
-      console.log(getClientRecordResult)
+      await  utils.shouldThrow(contractInstance.getClientRecord(accounts[1]));
+      
   })
+  it("should check if client records exist", async () => {
+      const doesClientRecordExistResult = await contractInstance.doesClientRecordExist(accounts[1]);
+
+      assert(doesClientRecordExistResult == false);
+  })
+
+  it("should get a client record", async () => {
+    
+    let clientRecord = await ClientRecordContract.deployed();
+
+  let savingsConfig =  await SavingsConfigContract.deployed();
+
+  let groups = await GroupsContract.deployed();
+
+  let esusuService = await EsusuServiceContract.deployed();
+
+  let rewardConfig = await RewardConfigContract.deployed(
+    esusuService.address,
+    groups.address
+  );
+
+  let xendToken = await xendTokenContract.deployed("Xend Token", "XTK", 18, 2000000);
+
+  let yXend = await yxendTokenContract.deployed("YXend Token", "YXTK", 18, 2000000);
+
+  let daiLendingService = await DaiLendingServiceContract.deployed();
+
+  let daiLendingAdapter = await DaiLendingAdapterContract.deployed(DaiLendingServiceContract.address);
+
+    const instance = await XendFinanceIndividual_Yearn_V1.new(
+      daiLendingAdapter.address,
+      daiLendingService.address,
+      xendToken.address,
+      clientRecord.address,
+      rewardConfig.address,
+      yXend.address
+    );
+
+
+  await clientRecord.activateStorageOracle(instance.address, {from :accounts[0]});
+
+  const getClientRecordResult = await instance.getClientRecord(accounts[2], {from : instance.address})
+
+  console.log(getClientRecordResult)
+
+  })
+
+
 });
