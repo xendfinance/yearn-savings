67ecd3d2c4b753aca0602116a2d653a28e50c53f
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-02-24 16:22:32 +0100

    updates..

--- Tests/contracts/XendFinanceIndividual_Yearn_V1.sol
+++ Tests/contracts/XendFinanceIndividual_Yearn_V1.sol
@@ -530,6 +530,8 @@ contract XendFinanceIndividual_Yearn_V1 is
             "Could not complete deposit process from token contract"
         );
 
+        LendingAdapterAddress = lendingService.GetDaiLendingAdapterAddress();
+
         daiToken.approve(LendingAdapterAddress, amountTransferrable);
 
         uint256 balanceBeforeDeposit = lendingService.userShares();
@@ -591,6 +593,8 @@ contract XendFinanceIndividual_Yearn_V1 is
             isSuccessful,
             "Could not complete deposit process from token contract"
         );
+
+        LendingAdapterAddress = lendingService.GetDaiLendingAdapterAddress();
 
         daiToken.approve(LendingAdapterAddress, amountTransferrable);
 
